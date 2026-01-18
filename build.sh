1a51843ae98547ece6b4c0eaa13dbbf4f1197244
Bovmelo <bovmelo@outlook.com>
2026-01-18 14:41:41 +0800

    v0.0.2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 sudo mkdir -p /usr/local/include/bits
-curl https://raw.githubusercontent.com/Bovmelo/ | sudo zsh
+curl https://raw.githubusercontent.com/Bovmelo/macos-bitsstdc--/refs/heads/main/stdc%2B%2B.h -o /usr/local/include/bits/stdc++.h
